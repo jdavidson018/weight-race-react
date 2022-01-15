bd2acdeb6d4b2d7124e246dbcc54c8459a37e325
Jack Davidson <jack@Jacks-MBP.home>
2022-01-15 00:46:57 -0600

    Revert

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-//import NavigationDrawer from './Components/NavigationDrawer';
-import CollapsableNavigationDrawer from './Components/CollapsableNavigationDrawer';
+import NavigationDrawer from './Components/NavigationDrawer';
+// import CollapsableNavigationDrawer from './Components/CollapsableNavigationDrawer';
 
 function App() {
   return (
     <div className="App">
-      <CollapsableNavigationDrawer></CollapsableNavigationDrawer>
-      {/* <NavigationDrawer></NavigationDrawer> */}
+      {/* <CollapsableNavigationDrawer></CollapsableNavigationDrawer> */}
+      <NavigationDrawer></NavigationDrawer>
     </div>
   );
 }
